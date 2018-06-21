78f09f165a07b3bd7dae5a20070ba623a8c09dba
Aliaksei Kisel <Aliaksei_Kisel@epam.com>
2018-06-21 17:45:10 +0300

    complete MainActivity

--- app/src/main/java/com/example/aliaksei_kisel/bluetoothtest/BluetoothConnectionRequester.kt
+++ app/src/main/java/com/example/aliaksei_kisel/bluetoothtest/BluetoothConnectionRequester.kt
@@ -1,23 +1,22 @@
 package com.example.aliaksei_kisel.bluetoothtest
 
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothHeadset
 import android.bluetooth.BluetoothProfile
 import android.content.Context
 
-class BluetoothConnectionRequester(private val callback: CallbackReceiver)
+class BluetoothConnectionRequester(private val callback: CallbackProxy)
     : BluetoothProfile.ServiceListener {
 
     override fun onServiceDisconnected(i: Int) {
-
+        //TODO disconnected
     }
 
     override fun onServiceConnected(i: Int, bluetoothProfile: BluetoothProfile?) {
-
+        callback.onHeadSetProxyReceived(bluetoothProfile as BluetoothHeadset)
     }
 
     fun request(context: Context, adapter: BluetoothAdapter) {
         adapter.getProfileProxy(context, this, BluetoothProfile.HEADSET)
     }
-
-
 }
--- app/src/main/java/com/example/aliaksei_kisel/bluetoothtest/MainActivity.kt
+++ app/src/main/java/com/example/aliaksei_kisel/bluetoothtest/MainActivity.kt
@@ -2,16 +2,22 @@ package com.example.aliaksei_kisel.bluetoothtest
 
 import android.Manifest
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothHeadset
 import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import com.example.aliaksei_kisel.bluetoothtest.utils.loge
+import com.example.aliaksei_kisel.bluetoothtest.utils.logi
 import com.example.aliaksei_kisel.bluetoothtest.utils.toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.reflect.InvocationTargetException
+import java.lang.reflect.Method
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CallbackProxy, CallbackReceiver {
 
     var bluetoothAdapter: BluetoothAdapter? = null
 
@@ -20,8 +26,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         buttonStartConnect.setOnClickListener {
-            outputTextView.append("Starting connect...")
-            startServer()
+            outputTextView.append("Starting connect...\n")
+            startConnect()
         }
 
         if (!checkPermission()) {
@@ -39,8 +45,71 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun startServer() {
+    private fun startConnect() {
+        onBluetoothConnected()
+    }
 
+    override fun onHeadSetProxyReceived(proxy: BluetoothHeadset) {
+        val connect = getConnectMethod()
+        val device = findBondedDeviceByName(bluetoothAdapter, DEVICE_NAME)
+
+        if (connect == null || device == null) {
+            logi(TAG, "[ No connect method or no device found ]")
+        }
+
+        try {
+            connect?.isAccessible = true
+            connect?.invoke(proxy, device)
+        } catch (e: InvocationTargetException) {
+            loge(TAG, "Unable to invoke connect(BluetoothDevice) method on proxy.")
+            e.printStackTrace()
+        } catch (e: IllegalAccessException) {
+            loge(TAG, "Illegal Access!")
+            e.printStackTrace()
+        }
+    }
+
+    private fun findBondedDeviceByName(adapter: BluetoothAdapter?, deviceName: String)
+            : BluetoothDevice? {
+        for (device in getBoundedDevices(adapter)) {
+            logi(TAG, "[ device_name = ${device.name}]")
+            if (deviceName == device.name) {
+                logi(TAG,
+                        "[ Found device with name = ${device.name} and address = ${device.address} ]")
+                return device
+            }
+        }
+        logi(TAG, "Unable to find device with name = $deviceName")
+        return null
+    }
+
+    private fun getBoundedDevices(adapter: BluetoothAdapter?): Set<BluetoothDevice> {
+        var results = adapter?.bondedDevices
+        if (results == null) {
+            results = HashSet<BluetoothDevice>()
+        }
+        return results
+    }
+
+    private fun getConnectMethod(): Method? {
+        try {
+            return BluetoothHeadset::class.java.getDeclaredMethod("connect",
+                    BluetoothDevice::class.java)
+        } catch (e: NoSuchMethodException) {
+            loge(TAG, "Unable to find connect(BluetoothDevice) method in BluetoothA2dp proxy.")
+            e.printStackTrace()
+            return null
+        }
+    }
+
+    override fun onBluetoothConnected() {
+        bluetoothAdapter?.let {
+            BluetoothConnectionRequester(this).request(this, it)
+        }
+    }
+
+    override fun onBluetoothError() {
+        loge(TAG, "There was an error enabling the Bluetooth Adapter.")
     }
 
     private fun checkPermission(): Boolean {
@@ -86,10 +155,11 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
 
+        const val TAG = "BluetoothTest"
         const val REQUEST_PERMISSION_BLUETOOTH = 33
         const val REQUEST_PERMISSION_BLUETOOTH_ADMIN = 34
         const val NAME = "BluetoothTest"
-        val APP_UUID = UUID.fromString(NAME)
+        //val APP_UUID = UUID.fromString(NAME)
         const val DEVICE_NAME = "SBH20"
     }
 }
